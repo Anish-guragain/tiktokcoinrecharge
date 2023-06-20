2fe83ecc01950c0a8d26eb5a177bd6213f1a1ab6
Anish-guragain <anishguragain8@gmail.com>
2023-06-20 22:24:48 +0545

    nice

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
     extend: {},
     fontFamily: {
       poppins: ["Poppins", "sans-serif"],
     },
-   
   },
   plugins: [],
-}
+};
